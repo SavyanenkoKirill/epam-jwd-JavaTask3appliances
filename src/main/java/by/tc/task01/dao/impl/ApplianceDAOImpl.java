1050a8f61f97eccbdd5434a8c55ead54866aa801
Savyan <mrsavyan555@gmail.com>
2022-03-24 17:39:41 +0300

    initial commit

--- src/main/java/by/tc/task01/dao/impl/ApplianceDAOImpl.java
+++ src/main/java/by/tc/task01/dao/impl/ApplianceDAOImpl.java
@@ -1,22 +1,25 @@
 package main.java.by.tc.task01.dao.impl;
 
 import main.java.by.tc.task01.dao.ApplianceDAO;
-import main.java.by.tc.task01.dao.exception.DAOException;
 import main.java.by.tc.task01.entity.Appliance;
 import main.java.by.tc.task01.entity.criteria.Criteria;
 import main.java.by.tc.task01.service.parser.ApplianceParser;
 import main.java.by.tc.task01.service.reader.ApplianceBaseReader;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class ApplianceDAOImpl implements ApplianceDAO {
-    private static final String APPLIANCES_DATABASE_FILE_PATH = "src/main/resources/appliances_db.txt";
+    //private static final String APPLIANCES_DATABASE_FILEPATH = "src/main/resources/appliances_db.txt";
+/*    private final String APPLIANCES_DATABASE_FILEPATH = Objects.requireNonNull(getClass().getClassLoader()
+            .getResource( " appliances_db.txt")).getPath();*/
+    private final String APPLIANCES_DATABASE_FILEPATH = new File("resources/appliances_db.txt").getAbsolutePath();
 
     @Override
     public List<Appliance> find(Criteria criteria) throws IOException {
-        List<String> applianceLines = ApplianceBaseReader.readApplianceBase(APPLIANCES_DATABASE_FILE_PATH);
+        List<String> applianceLines = ApplianceBaseReader.readApplianceBase(APPLIANCES_DATABASE_FILEPATH);
         List<Appliance> appliances = applianceLines.stream()
                 .map(ApplianceParser::parseApplianceLine)
                 .collect(Collectors.toList());
